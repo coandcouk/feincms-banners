ada530db46ea537293ee97e6fab5b853f24e9800
olu <olu@coandcouk.com>
2015-03-30 09:45:37 +0100

    Added migrations for adding banner target.

---
+++ feincms_banners/migrations/0002_auto__add_field_banner_target.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Banner.target'
+        db.add_column('feincms_banners_banner', 'target',
+                      self.gf('django.db.models.fields.CharField')(default=u'_self', max_length=20),
+                      keep_default=False)
+
+        # Adding M2M table for field sites on 'Banner'
+        db.create_table('feincms_banners_banner_sites', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('banner', models.ForeignKey(orm['feincms_banners.banner'], null=False)),
+            ('site', models.ForeignKey(orm['sites.site'], null=False))
+        ))
+        db.create_unique('feincms_banners_banner_sites', ['banner_id', 'site_id'])
+
+
+    def backwards(self, orm):
+        # Deleting field 'Banner.target'
+        db.delete_column('feincms_banners_banner', 'target')
+
+        # Removing M2M table for field sites on 'Banner'
+        db.delete_table('feincms_banners_banner_sites')
+
+
+    models = {
+        'feincms_banners.banner': {
+            'Meta': {'ordering': "[u'-active_from']", 'object_name': 'Banner'},
+            'active_from': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'active_until': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'code': ('django.db.models.fields.CharField', [], {'default': "u'y3sipef80nw4r3x75giazkwjk4d-8czudqjdp9nk'", 'unique': 'True', 'max_length': '40'}),
+            'embeds': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'impressions': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'mediafile': ('feincms.module.medialibrary.fields.MediaFileForeignKey', [], {'to': "orm['medialibrary.MediaFile']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['sites.Site']", 'symmetrical': 'False'}),
+            'target': ('django.db.models.fields.CharField', [], {'default': "u'_self'", 'max_length': '20'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        'feincms_banners.click': {
+            'Meta': {'ordering': "[u'-timestamp']", 'object_name': 'Click'},
+            'banner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'clicks'", 'to': "orm['feincms_banners.Banner']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'referrer': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'user_agent': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'})
+        },
+        'medialibrary.category': {
+            'Meta': {'ordering': "['parent__title', 'title']", 'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['medialibrary.Category']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '150'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'medialibrary.mediafile': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'MediaFile'},
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['medialibrary.Category']", 'null': 'True', 'blank': 'True'}),
+            'copyright': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '255'}),
+            'file_size': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '12'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['feincms_banners']
